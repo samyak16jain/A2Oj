7a9bfb7dd26d6e0cb60cfc11b2da44ae07a12d21
Samyak Jain <samyak16jain@gmail.com>
2020-06-12 21:50:01 +0530

    added ProblemTable
    props for this is any array element of Catagerys, DivisionLad,
    RatingLadders.
    
    impoted all 3 (cat,div,rating).js in shared

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import { Switch, Route, Redirect } from 'react-router-dom';
+
 import Header from './HeaderComponent';
 import Home from './HomeComponent';
+
+import { CATEGORIES } from '../shared/Problem_Category_wise';
+import { DIVISIONLADDERS } from '../shared/Problem_Division_Wise';
+import { RATINGLADDERS } from '../shared/Problem_Rating_Wise';
 
 class Main extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			categories: CATEGORIES,
+			div_ladder: DIVISIONLADDERS,
+			rate_ladder: RATINGLADDERS,
+		};
 	}
 
 	render() {
