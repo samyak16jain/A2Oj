72b58fc8a45bc8510a631c5b3a48e1bf8262336c
mrSandeepKr <sandeep.kumar.ece16@itbhu.ac.in>
2020-06-14 04:48:14 +0530

    split stores for each dataset  added

---
+++ src/redux/probCatWise.js
@@ -0,0 +1,8 @@
+import { CATEGORIES } from "../shared/Problem_Category_wise";
+
+export const ProbCatWise = (state = CATEGORIES, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
